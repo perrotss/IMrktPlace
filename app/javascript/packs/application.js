d622c27571180fbfdc7e0c7973c05d4dbd1dfef1
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2022-01-08 21:33:09 -0500

    need to finish code for cellphone images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require_self
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-import { window } from "../components/scripts.js";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,32 +16,31 @@ import { window } from "../components/scripts.js";
 // const imagePath = (name) => images(name, true)
 
 document.addEventListener("turbolinks:load", () => {
-    
-    window.addEventListener('DOMContentLoaded', event => {
-
-        // Activate Bootstrap scrollspy on the main nav element
-        const mainNav = document.body.querySelector('#mainNav');
-        if (mainNav) {
-            new bootstrap.ScrollSpy(document.body, {
-                target: '#mainNav',
-                offset: 74,
-            });
-        };
-    
-        // Collapse responsive navbar when toggler is visible
-        const navbarToggler = document.body.querySelector('.navbar-toggler');
-        const responsiveNavItems = [].slice.call(
-            document.querySelectorAll('#navbarResponsive .nav-link')
-        );
-        responsiveNavItems.map(function (responsiveNavItem) {
-            responsiveNavItem.addEventListener('click', () => {
-                if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                    navbarToggler.click();
-                }
-            });
-        });
-    
-    });
     // Call your functions here, e.g:
     // initSelect2();
+});
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 });
